81e4d83925d397955298ab0c9ab7c0fed9890736
BLOKKET-PLAYER <137057087+BLOKKET-PLAYER@users.noreply.github.com>
2024-10-17 12:48:42 +0100

    Update main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,23 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 10; index++) {
-        basic.showArrow(ArrowNames.North)
-        basic.showArrow(ArrowNames.NorthEast)
-        basic.showArrow(ArrowNames.East)
-        basic.showArrow(ArrowNames.SouthEast)
-        basic.showArrow(ArrowNames.South)
-        basic.showArrow(ArrowNames.SouthWest)
-        basic.showArrow(ArrowNames.West)
-        basic.showArrow(ArrowNames.NorthWest)
-    }
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 10; index++) {
-        basic.showIcon(IconNames.House)
-        basic.showIcon(IconNames.Tortoise)
-        basic.showIcon(IconNames.Scissors)
-        basic.showIcon(IconNames.Duck)
-        basic.showIcon(IconNames.Heart)
-    }
+    basic.showArrow(ArrowNames.NorthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.North)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showString("12345678910")
